97f9a8c5ee802d726788d651a6b0b5953239a34c
clutter <clutter.mk2@gmail.com>
2026-01-22 18:25:49 +0900

    abc083b

--- abs/src/bin/abc083b.rs
+++ abs/src/bin/abc083b.rs
@@ -2,11 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: i32,
         a: i32,
         b: i32,
-        c: i32,
-        s: String,
     }
+    let matched_num: Vec<i32> = (1..=n)
+        .map(|x| (x, sum_of_each_digit(x)))
+        .filter(|(_, s)| a <= *s && *s <= b)
+        .map(|(x, _)| x)
+        .collect();
+    println!("{}", matched_num.iter().sum::<i32>());
+}
 
-    println!("{} {}", a + b + c, s);
+fn sum_of_each_digit(x: i32) -> i32 {
+    let mi = x / 10000 % 10;
+    let th = x / 1000 % 10;
+    let hu = x / 100 % 10;
+    let te = x / 10 % 10;
+    let on = x % 10;
+    mi + th + hu + te + on
 }
