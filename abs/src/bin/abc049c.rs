ce585171934d6880c020dee62161cba9328908cd
clutter <clutter.mk2@gmail.com>
2026-01-27 12:46:18 +0900

    abs - abc049c

--- abs/src/bin/abc049c.rs
+++ abs/src/bin/abc049c.rs
@@ -2,11 +2,32 @@ use proconio::input;
 
 fn main() {
     input! {
-        a: i32,
-        b: i32,
-        c: i32,
         s: String,
     }
-
-    println!("{} {}", a + b + c, s);
+    // この順番でマッチさせれば手戻りなく進められるはず
+    let patterns = [
+        "dreameraser",
+        "dreamerase",
+        "dreamer",
+        "dream",
+        "eraser",
+        "erase",
+    ];
+    let mut t = String::new();
+    'outer: loop {
+        for p in patterns {
+            let temp = t.clone() + p;
+            if s.starts_with(&temp) {
+                t = temp;
+                continue 'outer;
+            }
+        }
+        // for でマッチするものがなければ抜ける
+        break;
+    }
+    if s == t {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
 }
