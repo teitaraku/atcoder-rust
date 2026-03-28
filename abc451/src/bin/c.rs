f6511bd0da28a8dad9c66e96034043098d731aba
clutter <clutter.mk2@gmail.com>
2026-03-29 00:32:57 +0900

    二分木を使って性能改善

--- abc451/src/bin/c.rs
+++ abc451/src/bin/c.rs
@@ -1,27 +1,32 @@
 use proconio::input;
+use std::collections::BTreeMap;
 
 fn main() {
     input! {
         q: usize,
     }
-    let mut command = vec![];
-    let mut height = vec![];
+
+    let mut tree_map: BTreeMap<usize, usize> = BTreeMap::new();
+    let mut total: usize = 0;
+
     for _ in 0..q {
         input! {
-            ci: usize,
-            hi: usize,
+            c: usize,
+            h: usize,
         }
-        command.push(ci);
-        height.push(hi);
-    }
 
-    let mut trees = vec![];
-    for i in 0..q {
-        if command[i] == 1 {
-            trees.push(height[i]);
+        if c == 1 {
+            *tree_map.entry(h).or_insert(0) += 1;
+            total += 1;
         } else {
-            trees.retain(|&h| height[i] < h);
+            // h+1 以上のキーを切り出し、残りは捨てる
+            let keep = tree_map.split_off(&(h + 1));
+            // 削除側を数えた方が効率的。残った方を数えると、次ループ以降も数えることになってしまう
+            let removed: usize = tree_map.values().sum();
+            total -= removed;
+            tree_map = keep;
         }
-        println!("{}", trees.len());
+
+        println!("{}", total);
     }
 }
